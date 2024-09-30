b34ce6f1df741b1cbc34b6dc2b3c84125409b871
MohammedYaseenRon <yaseenron3@gmail.com>
2024-10-01 01:18:55 +0530

    created empty  project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+   <div className="flex justify-center items-center h-screen bg-blue-400">
+      <h1 className="text-3xl text-white font-bold">Hey Boy</h1>
+   </div>
+  )
+}
+
+export default App
